f0c68b9bafb392a9c904dee873d55bced079bb5f
D-Moskalyov <dam@2memory.ru>
2024-07-21 18:54:57 +0300

    coverage

--- buildSrc/src/main/kotlin/io.github.idmosk.saga.kotlin-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/io.github.idmosk.saga.kotlin-library-conventions.gradle.kts
@@ -1,6 +1,7 @@
-import com.vanniktech.maven.publish.SonatypeHost
-import com.vanniktech.maven.publish.KotlinJvm
 import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinJvm
+import com.vanniktech.maven.publish.SonatypeHost
+import javax.xml.parsers.DocumentBuilderFactory
 
 plugins {
     id("io.github.idmosk.saga.kotlin-common-conventions")
@@ -20,6 +21,44 @@ tasks.register<Jar>("dokkaHtmlJar") {
     dependsOn(tasks.dokkaHtml)
     from(tasks.dokkaHtml.flatMap { it.outputDirectory })
     archiveClassifier.set("kdoc")
+}
+
+afterEvaluate {
+    if (tasks.findByName("koverXmlReport") != null) {
+        tasks.register("printLineCoverage") {
+            group = "verification" // Put into the same group as the `kover` tasks
+            dependsOn("koverXmlReport")
+            doLast {
+                val report = file("$buildDir/reports/kover/report.xml")
+
+                val doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(report)
+                val rootNode = doc.firstChild
+                var childNode = rootNode.firstChild
+
+                var coveragePercent = 0.0
+
+                while (childNode != null) {
+                    if (childNode.nodeName == "counter") {
+                        val typeAttr = childNode.attributes.getNamedItem("type")
+                        if (typeAttr.textContent == "LINE") {
+                            val missedAttr = childNode.attributes.getNamedItem("missed")
+                            val coveredAttr = childNode.attributes.getNamedItem("covered")
+
+                            val missed = missedAttr.textContent.toLong()
+                            val covered = coveredAttr.textContent.toLong()
+
+                            coveragePercent = (covered * 100.0) / (missed + covered)
+
+                            break
+                        }
+                    }
+                    childNode = childNode.nextSibling
+                }
+
+                println("%.1f".format(coveragePercent))
+            }
+        }
+    }
 }
 
 mavenPublishing {
